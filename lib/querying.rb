9119bc5944447d53c72fc714bcb3e7f5d7f4c0c5
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-13 02:47:06 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   "SELECT DISTINCT species
-  FROM characters "
+  FROM characters 
+  GROUP BY species"
 end
 
 def select_name_and_series_subgenres_of_authors
