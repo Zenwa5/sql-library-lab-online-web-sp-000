2229f2fc80dcf92f366c7d8cbb4cd2812c43c8ec
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-13 02:49:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT DISTINCT species, COUNT(species)
   FROM characters
-  ORDER BY species DESC
+  ORDER BY species ASC
   LIMIT 1 "
 end
 
