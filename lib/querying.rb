3c124caaad14057bc46e40ea8519efb236db2e5c
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2019-03-13 03:04:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON series.id = characters.series_id
   WHERE characters.species = 'human'
-  GROUP BY characters.species
+  GROUP BY series.title, characters.species
   ORDER BY species DESC
   LIMIT 1"
 end
